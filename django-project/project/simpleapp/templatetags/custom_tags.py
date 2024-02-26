ae5b5297e2ec1c3b468a8391791979c09c7054dc
Alexander <sadorsadoff@gmail.com>
2024-02-26 16:42:05 +0100

    d3

---
+++ django-project/project/simpleapp/templatetags/custom_tags.py
@@ -0,0 +1,11 @@
+from django.utils import timezone
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag()
+def current_time(format_string='%b %d %Y'):
+    return timezone.now().strftime(format_string)
